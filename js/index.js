967287b13cd6f0ff8d6c5228798eed489f2d8ea1
tiago <70700766+tiagozip@users.noreply.github.com>
2025-08-27 15:21:28 +0100

    kshdsgsds

--- js/index.js
+++ js/index.js
@@ -1,28 +1,46 @@
-import "./query.js";
-
 const defaultColor = "#71767b";
 const buttons = {
   accounts: {
     color: "#1da1f2",
-    toggled: true,
+    toggled: false,
   },
   tweets: {
     color: "#00ba7c",
-    toggled: true,
+    toggled: false,
   },
   lists: {
     color: "#7856ff",
-    toggled: true,
+    toggled: false,
   },
   ads: {
     color: "#ff7a00",
-    toggled: true,
+    toggled: false,
   },
 };
 
 const buttonElements = document.querySelectorAll(".toggle");
 const svgfill = (btn, color) => {
   btn.querySelector(".buttonicon").fill = color;
+};
+
+let searchQuery;
+
+const query = (text) => {
+  if (document.querySelector(".results")) {
+    document.querySelector(".results").remove();
+  }
+  if (!text) return;
+
+  document.querySelector(".searchbar").blur();
+
+  searchQuery = text;
+
+  const resultsEl = document.createElement("div");
+  resultsEl.className = "results";
+  resultsEl.innerText = `results for "${text}" @ ${Object.entries(buttons).find(([_, b]) => b.toggled)?.[0] || "accounts"} (not really)`;
+  document.querySelector(".center").appendChild(resultsEl);
+
+  history.pushState({}, "", `?q=${encodeURIComponent(text)}`);
 };
 
 buttonElements.forEach((btn, i) => {
@@ -60,6 +78,7 @@ buttonElements.forEach((btn, i) => {
 
     buttonElements.forEach((b) => {
       b.classList.remove("pressed");
+      buttons[b.getAttribute("data-toggle")].toggled = false;
 
       svgfill(b, defaultColor);
       b.style.color = defaultColor;
@@ -71,8 +90,58 @@ buttonElements.forEach((btn, i) => {
 
       btn.style.color = buttons[type].color;
       svgfill(btn, buttons[type].color);
+
+      if (document.querySelector(".results")) {
+        query(document.querySelector(".searchbar").value.trim());
+      }
     }
   });
 
   if (i === 0) btn.click();
 });
+
+document.querySelector(".searchbar").addEventListener("keydown", (e) => {
+  if (e.key !== "Enter") return;
+  e.preventDefault();
+
+  const queryText = e.target.value.trim();
+
+  if (queryText.length === 0) return;
+  query(queryText);
+});
+
+window.addEventListener("popstate", () => {
+  const params = new URLSearchParams(window.location.search);
+  const q = params.get("q") || "";
+  document.querySelector(".searchbar").value = q;
+
+  if (q.length > 0) {
+    query(q);
+  } else if (document.querySelector(".results")) {
+    document.querySelector(".results").remove();
+    searchQuery = null;
+  }
+});
+
+if (location.search.startsWith("?q=")) {
+  const params = new URLSearchParams(window.location.search);
+  const q = params.get("q") || "";
+  document.querySelector(".searchbar").value = q;
+
+  if (q.length > 0) {
+    query(q);
+  }
+}
+
+document.querySelector(".logo").addEventListener("click", () => {
+  history.pushState({}, "", "/");
+  document.querySelector(".searchbar").value = "";
+  if (document.querySelector(".results")) {
+    document.querySelector(".results").remove();
+    searchQuery = null;
+  }
+});
+
+setInterval(() => {
+  document.title = `${searchQuery ? `${searchQuery} -` : `(${Math.floor(Math.random() * 900) + 100}) /`} twitter.cat`;
+}, 100);
