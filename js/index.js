32f092e4e7c4893b20232dea05e5ea75b150daf7
tiago <70700766+tiagozip@users.noreply.github.com>
2025-08-27 15:40:38 +0100

    loading spinner!

--- js/index.js
+++ js/index.js
@@ -37,7 +37,12 @@ const query = (text) => {
 
   const resultsEl = document.createElement("div");
   resultsEl.className = "results";
-  resultsEl.innerText = `results for "${text}" @ ${Object.entries(buttons).find(([_, b]) => b.toggled)?.[0] || "accounts"} (not really)`;
+  resultsEl.innerHTML = `<div style="text-align:center;margin-top:.5em"><svg width="28" height="28" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .5s infinite linear}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25" fill="#1EA1F1"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" fill="#1EA1F1" class="spinner_ajPY"/></svg></div>`;
+
+  setTimeout(() => {
+    resultsEl.innerHTML = `${Object.entries(buttons).find(([_, b]) => b.toggled)?.[0] || "accounts"} for "${text}"`;
+  }, 300);
+
   document.querySelector(".center").appendChild(resultsEl);
 
   history.pushState({}, "", `?q=${encodeURIComponent(text)}`);
