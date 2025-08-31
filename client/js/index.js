658304d53b06270f393340c67216648f616407ac
tiago <70700766+tiagozip@users.noreply.github.com>
2025-08-31 19:59:34 +0100

    nice card

--- client/js/index.js
+++ client/js/index.js
@@ -27,6 +27,14 @@ const svgfill = (btn, color) => {
 
 let searchQuery;
 
+function formatNumber(num) {
+  if (!num) return "0";
+  if (num < 1000) return String(num);
+  if (num < 1_000_000) return `${Math.floor(num / 1000)}k`;
+  if (num < 1_000_000_000) return `${Math.floor(num / 1_000_000)}M`;
+  return `${Math.floor(num / 1_000_000_000)}B`;
+}
+
 const query = async (text) => {
   if (document.querySelector(".results")) {
     document.querySelector(".results").remove();
@@ -90,10 +98,14 @@ const query = async (text) => {
         avatar.loading = "lazy";
         avatar.src =
           result.avatar?.replaceAll("_normal.jpg", "_bigger.jpg") ||
-          "assets/svgs/default-avatar.png";
+          "https://abs.twimg.com/sticky/default_profile_images/default_profile_bigger.png";
+        if (result.square_avatar) {
+          avatar.style.borderRadius = "2px";
+        }
         avatar.onerror = function () {
           this.onerror = null;
-          this.src = "assets/svgs/default-avatar.png";
+          this.src =
+            "https://abs.twimg.com/sticky/default_profile_images/default_profile_bigger.png";
         };
 
         const info = document.createElement("div");
@@ -102,6 +114,8 @@ const query = async (text) => {
         const nameDiv = document.createElement("div");
         nameDiv.className = "name";
         nameDiv.textContent = result.name || result.username;
+
+        console.log(result);
 
         if (result.verified) {
           const verifiedSvg = document.createElementNS(
@@ -137,12 +151,65 @@ const query = async (text) => {
 
         info.appendChild(nameDiv);
         info.appendChild(usernameDiv);
-        info.appendChild(bioDiv);
 
+        const stats = document.createElement("div");
+        stats.className = "stats";
+        stats.innerHTML = `
+      <div class="stat">
+        <div class="stat-number">${formatNumber(result.followers)}</div>
+        <div class="stat-label">followers</div>
+      </div>
+      <div class="stat">
+        <div class="stat-number">${formatNumber(result.following)}</div>
+        <div class="stat-label">following</div>
+      </div>
+      <div class="stat">
+        <div class="stat-number">${formatNumber(result.tweets)}</div>
+        <div class="stat-label">tweets</div>
+      </div>
+      ${
+        result.location
+          ? `<div class="stat">
+        <div class="stat-label"><svg viewBox="0 0 24 24" aria-hidden="true" width="18px"><g><path d="M12 7c-1.93 0-3.5 1.57-3.5 3.5S10.07 14 12 14s3.5-1.57 3.5-3.5S13.93 7 12 7zm0 5c-.827 0-1.5-.673-1.5-1.5S11.173 9 12 9s1.5.673 1.5 1.5S12.827 12 12 12zm0-10c-4.687 0-8.5 3.813-8.5 8.5 0 5.967 7.621 11.116 7.945 11.332l.555.37.555-.37c.324-.216 7.945-5.365 7.945-11.332C20.5 5.813 16.687 2 12 2zm0 17.77c-1.665-1.241-6.5-5.196-6.5-9.27C5.5 6.916 8.416 4 12 4s6.5 2.916 6.5 6.5c0 4.073-4.835 8.028-6.5 9.27z" fill="currentColor"></path></g></svg> ${result.location?.split("")?.join("")?.replaceAll("<", "&lt;")?.replaceAll(">", "&gt;")}</div>
+      </div>`
+          : ""
+      }
+      <div class="stat time-stat">
+        <div class="stat-label"><svg viewBox="0 0 24 24" aria-hidden="true" width="18px"><g><path d="M7 4V3h2v1h6V3h2v1h1.5C19.89 4 21 5.12 21 6.5v12c0 1.38-1.11 2.5-2.5 2.5h-13C4.12 21 3 19.88 3 18.5v-12C3 5.12 4.12 4 5.5 4H7zm0 2H5.5c-.27 0-.5.22-.5.5v12c0 .28.23.5.5.5h13c.28 0 .5-.22.5-.5v-12c0-.28-.22-.5-.5-.5H17v1h-2V6H9v1H7V6zm0 6h2v-2H7v2zm0 4h2v-2H7v2zm4-4h2v-2h-2v2zm0 4h2v-2h-2v2zm4-4h2v-2h-2v2z" fill="currentColor"></path></g></svg></svg> ${(new Date(result.created_at)).toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric" })}</div>
+      </div>
+      `;
+
+        if (result.bio) info.appendChild(bioDiv);
+
+        info.appendChild(stats);
         el.appendChild(avatar);
         el.appendChild(info);
 
         resultsEl.appendChild(el);
+
+        el.querySelector(".time-stat").addEventListener("mouseenter", () => {
+          const timeCard = document.createElement("div");
+          timeCard.className = "time-card";
+          timeCard.style.position = "absolute";
+          timeCard.style.top = `${el.querySelector(".time-stat").getBoundingClientRect().top + 8}px`;
+
+          timeCard.style.left = `${el.querySelector(".time-stat").getBoundingClientRect().left}px`;
+          timeCard.style.transform = "translateY(calc(-100% - 18px))";
+          timeCard.style.zIndex = "1000";
+          timeCard.innerHTML = `
+          <small>joined</small>
+          <p>${(new Date(result.created_at)).toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" })}</p>
+          <small>indexed</small>
+          <p>${(new Date(result.added_at)).toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" })}</p>
+          `;
+
+          el.querySelector(".time-stat").appendChild(timeCard);
+        });
+
+        el.querySelector(".time-stat").addEventListener("mouseleave", () => {
+          if (el.querySelector(".time-card"))
+            el.querySelector(".time-card").remove();
+        });
       }
     });
   } catch (e) {
