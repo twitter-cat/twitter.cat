c1b4b88c66b534636a2aedf3cc594630e83527cb
tiago <70700766+tiagozip@users.noreply.github.com>
2025-11-02 12:09:55 +0000

    add legal stuff

--- client/js/stats.js
+++ client/js/stats.js
@@ -1,35 +1,39 @@
 window.addEventListener("load", () => {
   setTimeout(async () => {
-    const API_URL =
-      window.API_URL ||
-      (await import("./config.js").then(({ API_URL }) => API_URL));
+    try {
+      const API_URL =
+        window.API_URL ||
+        (await import("./config.js").then(({ API_URL }) => API_URL));
 
-    const [accounts, tweets] = await (await fetch(`${API_URL}/stats`)).json();
+      const [accounts, tweets] = await (await fetch(`${API_URL}/stats`)).json();
 
-    document.querySelector(".counter .count.accounts p").innerText =
-      Number(accounts).toLocaleString();
-    document.querySelector(".counter .count.tweets p").innerText =
-      Number(tweets).toLocaleString();
+      document.querySelector(".counter .count.accounts p").innerText =
+        Number(accounts).toLocaleString();
+      document.querySelector(".counter .count.tweets p").innerText =
+        Number(tweets).toLocaleString();
 
-    document.querySelector(".counter").style.opacity = 1;
+      document.querySelector(".counter").style.opacity = 1;
 
-    setInterval(async () => {
-      const [addAccountsMax, addTweetsMax] = [10, 40];
+      setInterval(async () => {
+        const [addAccountsMax, addTweetsMax] = [10, 40];
 
-      document.querySelector(".counter .count.accounts p").innerText = Number(
-        parseInt(
-          document
-            .querySelector(".counter .count.accounts p")
-            .innerText.replaceAll(",", "")
-        ) + Math.floor(Math.random() * addAccountsMax)
-      ).toLocaleString();
-      document.querySelector(".counter .count.tweets p").innerText = Number(
-        parseInt(
-          document
-            .querySelector(".counter .count.tweets p")
-            .innerText.replaceAll(",", "")
-        ) + Math.floor(Math.random() * addTweetsMax)
-      ).toLocaleString();
-    }, 3000);
+        document.querySelector(".counter .count.accounts p").innerText = Number(
+          parseInt(
+            document
+              .querySelector(".counter .count.accounts p")
+              .innerText.replaceAll(",", "")
+          ) + Math.floor(Math.random() * addAccountsMax)
+        ).toLocaleString();
+        document.querySelector(".counter .count.tweets p").innerText = Number(
+          parseInt(
+            document
+              .querySelector(".counter .count.tweets p")
+              .innerText.replaceAll(",", "")
+          ) + Math.floor(Math.random() * addTweetsMax)
+        ).toLocaleString();
+      }, 3000);
+    } catch (e) {
+      console.warn(e);
+    }
   }, 15);
 });
