0fbfb088ee93b0f297aebce7eac1dee2ac34908d
tiago <70700766+tiagozip@users.noreply.github.com>
2025-12-13 20:44:01 +0000

    new live page!!!

--- server/src/stats.js
+++ server/src/stats.js
@@ -7,7 +7,7 @@ const postgresReadOnly = new SQL(
 
 let statsCache = null;
 let statsCacheTime = 0;
-const STATS_CACHE_TTL = 30000;
+const STATS_CACHE_TTL = 30_000;
 
 export default new Elysia().get("/stats", async () => {
   const now = Date.now();
@@ -16,18 +16,71 @@ export default new Elysia().get("/stats", async () => {
     return statsCache;
   }
 
-  statsCache = (
-    await Promise.all([
-      postgresReadOnly`
-    SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'profiles';
-  `,
-      postgresReadOnly`
-   SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'tweets';
-  `,
-    ])
-  ).map((res) => Number(res?.[0]?.estimate || 0));
+  const [row] = await postgresReadOnly`
+  SELECT
+    MAX(reltuples::bigint) FILTER (WHERE relname = 'profiles') AS profiles,
+    MAX(reltuples::bigint) FILTER (WHERE relname = 'tweets') AS tweets
+  FROM pg_class
+  WHERE relname IN ('profiles', 'tweets');
+`;
 
+  statsCache = [
+    Number(row?.profiles || 0),
+    Number(row?.tweets || 0),
+  ];
   statsCacheTime = now;
 
   return statsCache;
+}).get("/live/lh", async () => {
+  const [lastHour] = await postgresReadOnly`
+  SELECT COUNT(*) AS count
+FROM tweets
+WHERE added_at > NOW() - INTERVAL '1 hour';`;
+
+  return {
+    lastHour: lastHour?.count || 0,
+  };
+}).get("/live/tweets", async ({ query }) => {
+  const { hash, cursor } = query;
+
+  let tweets;
+
+  if (cursor) {
+    tweets = await postgresReadOnly`SELECT
+      t.author_id, t.body, t.id, t.added_at, t.media,
+      p.name, p.username, p.avatar
+    FROM tweets t
+    LEFT JOIN profiles p
+    ON p.id = t.author_id
+    WHERE t.added_at < ${cursor}
+    ORDER BY t.added_at DESC
+    LIMIT 60`;
+  } else {
+    tweets = await postgresReadOnly`SELECT
+      t.author_id, t.body, t.id, t.added_at, t.media,
+      p.name, p.username, p.avatar
+    FROM tweets t
+    LEFT JOIN profiles p
+    ON p.id = t.author_id
+    ORDER BY t.added_at DESC
+    LIMIT 60`;
+  }
+
+  const str = JSON.stringify(tweets, Object.keys(tweets).sort());
+  let h = 2166136261;
+  for (let i = 0; i < str.length; i++) {
+    h ^= str.charCodeAt(i);
+    h = Math.imul(h, 16777619);
+  }
+
+  const hhash = h >>> 0;
+
+  if (hash && (hash === hhash.toString())) {
+    return { hash: hhash };
+  }
+
+  return {
+    tweets,
+    hash: hhash
+  };
 });
