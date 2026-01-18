4d8ce21da02d6931ebdc439a4deb974585e518e1
tiago <70700766+tiagozip@users.noreply.github.com>
2026-01-18 19:17:45 +0000

    update api url

--- client/js/config.js
+++ client/js/config.js
@@ -1,6 +1,6 @@
 export const API_URL =
   location.hostname === "localhost"
     ? "http://localhost:3001"
-    : "https://twittercat.tiagorangel.com";
+    : "https://api-prod-twittercat.tiago.zip";
 
 window.API_URL = API_URL;
